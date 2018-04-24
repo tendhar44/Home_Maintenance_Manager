3b274e7090dc2d5e03b571464c4984ba270fa2fc
khouathao <khoua1992@gmail.com>
2018-04-24 02:28:59 -0500

    fix applaince delete back link

--- app/views/delete-appliance-page.php
+++ app/views/delete-appliance-page.php
@@ -1,4 +1,4 @@
 <div class="container" id="info">
 
-<a href="/home_maintenance_manager/public/appliancecontroller/<?php echo $_SESSION['propertyid' . $data['an']]; ?>"><-- Back to Appliance List</a>
+<a href="/home_maintenance_manager/public/appliancecontroller/<?php echo $_SESSION['applianceId' . $data['an']]['propertyId']; ?>"><-- Back to Appliance List</a>
 </div>
