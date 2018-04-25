77ac38b721bc4ba3d9157f79e0a143883089e277
khouathao <khoua1992@gmail.com>
2018-04-24 19:33:22 -0500

    fix create user comfirm password

--- app/views/add-user-page.php
+++ app/views/add-user-page.php
@@ -1,3 +1,29 @@
+
+
+<script type="text/javascript">
+
+function validatePassword(){
+    var password = document.getElementById("password");
+    var confirm_password = document.getElementById("confirmPassword");
+
+    if (password.value == '' && confirm_password.value == ''){
+        document.getElementById('check').innerHTML = '';
+        return false;
+    }
+
+    if(password.value != confirm_password.value) {
+        document.getElementById('check').style.color = 'red';
+        document.getElementById('check').innerHTML = 'Password doesn\'t match';
+        return false;
+    } else {
+        document.getElementById('check').style.color = 'green';
+        document.getElementById('check').innerHTML = 'match';
+        return true;
+    }
+}
+
+</script>
+
 <div class="container">
     <h3>Create A User for <?php echo $_SESSION['username']; ?></h3>
     <hr>
@@ -76,7 +102,7 @@
             </tr>
             <tr>
                 <td>Password:&nbsp;<span class="reqAsk">*</span></td>
-                <td><input type="password" name="password" required></td>
+                <td><input type="password" onchange="validatePassword()" name="password" id="password" required></td>
             </tr>
             <tr>
                 <td></td>
@@ -89,7 +115,7 @@
             </tr>
             <tr>
                 <td>Confirm Password:&nbsp;<span class="reqAsk">*</span></td>
-                <td><input type="password" name="passwordConfirm" required></td>
+                <td><input type="password" onkeyup="validatePassword()" name="confirmPassword" id="confirmPassword" required></td>
             </tr>
             <tr>
                 <td><input type="hidden" name="ownerid" value="<?php echo $data['useId']; ?>"></td>
@@ -106,8 +132,25 @@
         </table>
     </form>
 
+                    
+                <div class="form-group">
+                    <span id="check"></span>
+                </div>
+
     <div>
         <br><br><br><br>
         <span class="reqAsk">*</span> = required
     </div>
 </div>
+
+
+<script>
+$(function(){
+    $("#myform").submit(function(e){
+        debugger;
+        if(!validatePassword()){
+            e.preventDefault();
+        }
+    });
+});
+</script>
